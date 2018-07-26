11be4bbd84bafe2b7e0d8fab28b0b7d5d68c3390
Taylor Radtke <taylorradtke@gmail.com>
2018-07-25 20:53:27 -0400

    first commit

---
+++ dist/site.js
@@ -0,0 +1,21 @@
+/*!
+ * DO NOT OVERRIDE THIS FILE.
+ * Generated with `npm run build`
+ *
+ * wdg-wordpress-theme - WDG's opinionated WordPress theme
+ * @version 0.7.0
+ * @author WDG
+ * @link https://www.webdevelopmentgroup.com/
+ */
+
+
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+	typeof define === 'function' && define.amd ? define(['exports'], factory) :
+	(factory((global.Site = global.Site || {})));
+}(this, (function (exports) { 'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+})));
+//# sourceMappingURL=site.js.map
